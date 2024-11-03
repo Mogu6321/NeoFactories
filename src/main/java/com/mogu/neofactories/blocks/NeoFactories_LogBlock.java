a15e5fa216be59308a2bfe318acd4baf51ea699b
Mogu6321 <motimotikurage@hotmail.com>
2024-11-04 00:54:55 +0900

    SOLICA_LOGの皮を剥げるように

--- src/main/java/com/mogu/neofactories/blocks/NeoFactories_LogBlock.java
+++ src/main/java/com/mogu/neofactories/blocks/NeoFactories_LogBlock.java
@@ -1,15 +1,31 @@
 package com.mogu.neofactories.blocks;
 
-import net.minecraft.world.level.block.Block;
+
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.Property;
+import net.neoforged.neoforge.common.ItemAbilities;
+import net.neoforged.neoforge.common.ItemAbility;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class NeoFactories_LogBlock extends RotatedPillarBlock{
     public NeoFactories_LogBlock(BlockBehaviour.Properties properties) {
         super(properties);
-        this.registerDefaultState((BlockState)this.defaultBlockState());
+        this.registerDefaultState(this.defaultBlockState());
+    }
+    public @Nullable BlockState getToolModifiedState(@NotNull BlockState state, @NotNull UseOnContext context, @NotNull ItemAbility itemAbility, boolean simulate) {
+        if (itemAbility == ItemAbilities.AXE_STRIP) {
+            if (state.is(NeoFactories_blocks.SOLICA_LOG.get())) {
+                return(NeoFactories_blocks.STRIPPED_SOLICA_LOG.get()).defaultBlockState().setValue(AXIS,state.getValue(AXIS));
+            }
+
+            if (state.is(NeoFactories_blocks.SOLICA_LOG.get())) {
+                return(NeoFactories_blocks.STRIPPED_SOLICA_LOG.get()).defaultBlockState().setValue(AXIS,state.getValue(AXIS));
+            }
+        }
+
+        return super.getToolModifiedState(state, context, itemAbility, simulate);
     }
 }
