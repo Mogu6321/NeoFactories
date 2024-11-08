b9814708fbcda19839dd028f682cad3f159c4268
Mogu6321 <motimotikurage@hotmail.com>
2024-11-08 22:35:49 +0900

    コード修正

--- src/main/java/com/mogu/neofactories/blocks/NeoFactories_FacingPillarBlock.java
+++ src/main/java/com/mogu/neofactories/blocks/NeoFactories_FacingPillarBlock.java
@@ -11,33 +11,33 @@ import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.Property;
+import org.jetbrains.annotations.NotNull;
 
 public class NeoFactories_FacingPillarBlock extends DirectionalBlock{
     public static final MapCodec<NeoFactories_FacingPillarBlock> CODEC = simpleCodec(NeoFactories_FacingPillarBlock::new);
 
     public NeoFactories_FacingPillarBlock(BlockBehaviour.Properties properties) {
         super(properties);
-        this.registerDefaultState((BlockState)this.defaultBlockState().setValue(FACING, Direction.UP));
+        this.registerDefaultState(this.defaultBlockState().setValue(FACING, Direction.UP));
     }
 
-    protected MapCodec<? extends DirectionalBlock> codec() {
+    protected @NotNull MapCodec<? extends DirectionalBlock> codec() {
         return CODEC;
     }
 
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(new Property[]{FACING});
+        builder.add(FACING);
     }
 
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return (BlockState)this.defaultBlockState().setValue(FACING, context.getClickedFace());
+        return this.defaultBlockState().setValue(FACING, context.getClickedFace());
     }
 
-    public BlockState rotate(BlockState state, Rotation rotation) {
-        return (BlockState)state.setValue(FACING, rotation.rotate((Direction)state.getValue(FACING)));
+    public @NotNull BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
     }
 
-    public BlockState mirror(BlockState state, Mirror mirror) {
-        return (BlockState)state.setValue(FACING, mirror.mirror((Direction)state.getValue(FACING)));
+    public @NotNull BlockState mirror(BlockState state, Mirror mirror) {
+        return state.setValue(FACING, mirror.mirror(state.getValue(FACING)));
     }
 }
