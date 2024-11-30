0e9ffc0f5a9d73a3a2a2c381495d7425e53ab36f
Mogu6321 <motimotikurage@hotmail.com>
2024-11-30 23:18:04 +0900

    DataMapの追加

--- src/main/java/com/mogu/neofactories/datagen/NeoFactoriesDataMapProvider.java
+++ src/main/java/com/mogu/neofactories/datagen/NeoFactoriesDataMapProvider.java
@@ -1,8 +1,12 @@
 package com.mogu.neofactories.datagen;
 
+import com.mogu.neofactories.blocks.NeoFactories_Block;
+import com.mogu.neofactories.items.NeoFactories_Item;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DataMapProvider;
+import net.neoforged.neoforge.registries.datamaps.builtin.Compostable;
+import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -13,6 +17,8 @@ public class NeoFactoriesDataMapProvider extends DataMapProvider {
 
     @Override
     protected void gather() {
-
+        this.builder(NeoForgeDataMaps.COMPOSTABLES)
+                .add(NeoFactories_Block.SOLICA_LEAVES.getId(), new Compostable(0.3f), false)
+                .add(NeoFactories_Item.SOLIUM.getId(), new Compostable(0.5f), false);
     }
 }
